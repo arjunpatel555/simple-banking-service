52257108bc44673acf8dc425bdc9817056738648
Arjun Patel <arjunpatel555@gmail.com>
2023-05-05 10:35:26 +1000

    clean up prints

--- lib/main.rb
+++ lib/main.rb
@@ -8,15 +8,21 @@ shell_input_args = ARGV
 file_balances = shell_input_args[0]
 file_transactions = shell_input_args[1]
 
-# balances
+def print_company_balances(company)
+  company.accounts.each{ |account| print"acc_number: #{account.account_number} bal: #{account.balance}\n"}
+end
+
+# load balances
 print "Loading balance from #{file_balances}\n"
 company = Company.new("Alpha Sales")
 Transactions.load_company_balances(company, file_balances)
 print "Company Balances:\n"
-company.accounts.each{ |account| print"acc_number: #{account.account_number} bal: #{account.balance}\n"}
+print_company_balances(company)
 
-# transactions
+# process transactions
 print "Processing Transactions from #{file_transactions}\n"
 Transactions.process_company_transactions(company, file_transactions)
 print "Updated Company Balances:\n"
-company.accounts.each{ |account| print"acc_number: #{account.account_number} bal: #{account.balance}\n"}
+print_company_balances(company)
+
+
